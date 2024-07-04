9ed442f6c44afd9676a2839d8e8d2ab1b1c3002e
taiwosugar <oguntoyinborichman@gmail.com>
2024-07-04 03:43:21 +0100

    fd

--- src/componets/Footer.jsx
+++ src/componets/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import "./Footer.css"
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
@@ -47,13 +49,19 @@ const Footer = () => {
     </div>
     <div className="footer-ul-mobile">
       <h4>BOOK YOUR FLIGHT NOW</h4>
+      <ul className='footer-media'>
+      <Link
+       to={"https://instagram.com/diamondvisa/"}><li><FaInstagram/></li></Link>
+      <Link to={"https://facebook.com/diamondvisa"}><li><FaFacebook/></li></Link>
+      <Link to={"(link unavailable)"}><li><FaTwitter/></li></Link>
+    </ul>
         <ul>
           
-          <li>ABOUT</li>
-          <li>SERVICES</li>
-          <li>FLIGHTS</li>
-          <li>CONTACT</li>
-          <li>FAQ</li>
+          <Link to={"/about-us"}><li>ABOUT</li></Link>
+          <Link><li>SERVICES</li></Link>
+          <Link><li>FLIGHTS</li></Link>
+          <Link> <li>CONTACT</li></Link>
+          <Link><li>FAQ</li></Link>
         </ul>
         <p>Copyright 2024 @ <b>Diamond Visa</b>.</p>
       </div>
