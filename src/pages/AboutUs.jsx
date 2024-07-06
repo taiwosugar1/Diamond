4b2cc56db04c6fadd40bc6841210b7708d6ed9b1
taiwosugar1 <150476957+taiwosugar1@users.noreply.github.com>
2024-07-06 17:55:13 +0100

    Update AboutUs.jsx

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -91,6 +91,7 @@ Empowering your global journey with expert visa and immigration solutions.
             </div>
            
         </div>
+        <p>Copyright 2024 © <b>Diamond Visa and immigration services </b><br/>All rights reserved.</p>
       </footer>
     </div>
   );
