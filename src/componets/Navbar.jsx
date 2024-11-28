7f6c1510a26a78d7764a572eb3f28f5a81276006
taiwosugar <oguntoyinborichman@gmail.com>
2024-11-28 09:29:15 +0100

    fvhg

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
       </div>
       <ul className={`navbar-menu ${isOpen ? 'open' : ''}`}>
       <li><Link to="/login" onClick={closeMenu}>Login</Link></li>
-      <li><Link to="/hotel" onClick={closeMenu}>Hotel</Link></li>
+      <li><Link to="/tour" onClick={closeMenu}>Tourists</Link></li>
         <li><Link to="/" onClick={closeMenu}>Home</Link></li>
         <li><Link to="/about" onClick={closeMenu}>About</Link></li>
         <li><Link to="/contact" onClick={closeMenu}>Contact</Link></li>
